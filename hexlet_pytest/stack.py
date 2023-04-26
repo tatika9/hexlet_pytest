901e31181db40c9c4d59224052812a35f7f89554
Rinat Shakirov <tatika0872@gmail.com>
2023-04-26 16:11:15 +0500

    add test stack

---
+++ hexlet_pytest/stack.py
@@ -0,0 +1,8 @@
+stack = []
+stack.append(1)  # [1]
+stack.append(2)  # [1, 2]
+stack.append(3)  # [1, 2, 3]
+
+stack.pop()  # В стеке [1, 2]
+stack.pop()  # В стеке [1]
+stack.pop()  # В стеке пусто
